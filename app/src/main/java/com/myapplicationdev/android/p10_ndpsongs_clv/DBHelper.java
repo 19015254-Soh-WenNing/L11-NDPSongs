f5f0c351e0c3f6837f0633e8bf61987e970ffd52
19015254 <19015254@myrp.edu.sg>
2021-08-10 15:06:01 +0800

    added blocksDescendants

--- app/src/main/java/com/myapplicationdev/android/p10_ndpsongs_clv/DBHelper.java
+++ app/src/main/java/com/myapplicationdev/android/p10_ndpsongs_clv/DBHelper.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 public class DBHelper extends SQLiteOpenHelper {
 
 	private static final String DATABASE_NAME = "ndpsongs.db";
-	private static final int DATABASE_VERSION = 1;
+	private static final int DATABASE_VERSION = 2;
 	private static final String TABLE_SONG = "Song";
 	private static final String COLUMN_ID = "_id";
 	private static final String COLUMN_TITLE = "title";
